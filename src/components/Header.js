17debcd4edfdb3f0fa03f31eb69f980eb236b2f8
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2019-05-07 14:26:00 +0200

    add panda videos with markdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import styles from './Header.module.css';
 
 const ListLink = props => (
-    <li className={styles.listItem}>
-        <Link to={props.to}>{ props.children }</Link>
-    </li>
+  <li className={styles.listItem}>
+    <Link to={props.to}>{ props.children }</Link>
+  </li>
 );
 
-export default () => (
+export default () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  );
+
+  return (
     <header className={styles.container}>
-        <Link to="/" className={styles.siteLink}>
-            <h3 className={styles.siteTitle}>Pandas Eating Lots</h3>
-        </Link>
-        <ul className={styles.list}>
-            <ListLink to="/about/">About</ListLink>
-        </ul>
+      <Link to="/" className={styles.siteLink}>
+        <h3 className={styles.siteTitle}>{ data.site.siteMetadata.title }</h3>
+      </Link>
+      <ul className={styles.list}>
+        <ListLink to="/about/">About</ListLink>
+      </ul>
     </header>
-);
+  );
+};
